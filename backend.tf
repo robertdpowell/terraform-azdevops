3a8d1aa11ccebc052e2276249fe15c8ef8039bcf
robertdpowell <robertdpowell@gmail.com>
2024-04-01 21:16:37 +0100

    backend.tf dummy values

---
+++ backend.tf
@@ -0,0 +1,9 @@
+  backend "azurerm" {
+    # These fields are just placeholders for documentation purposes
+    # The actual values will be supplied by the -backend-config parameters
+    resource_group_name   = "placeholder"
+    storage_account_name  = "placeholder"
+    container_name        = "placeholder"
+    key                   = "placeholder"
+  }
+}
